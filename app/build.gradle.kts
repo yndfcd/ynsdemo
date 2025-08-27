57c3d190438d5a21adf7fa7d272359888e8f1c91
dengfeng.yan <dengfeng.yan@iherb.com>
2025-08-27 08:37:57 +0000

    feat: Add navigation-compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
