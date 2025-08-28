eaa9cfe2ff32fc4667167276dd61a57db850e528
dengfeng.yan <dengfeng.yan@iherb.com>
2025-08-28 09:04:14 +0000

    feat: added ktor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,10 @@ dependencies {
     testImplementation(libs.junit)
  testImplementation("io.mockk:mockk:1.13.11")
     testImplementation(libs.kotlinx.coroutines.test)
+ implementation(libs.ktor.client.serialization)
+
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.android)
     implementation(libs.kotlinx.coroutines.core)
 
     implementation(libs.androidx.core.ktx)
--- app/src/main/java/com/example/ynsdemo/cart/CartViewModel.kt
+++ app/src/main/java/com/example/ynsdemo/cart/CartViewModel.kt
@@ -1,10 +1,20 @@
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
 import kotlinx.coroutines.flow.update
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 data class CartItem(val id: String, val name: String, val price: Double)
-
 class CartViewModel {
 
     private val _cartItems = MutableStateFlow<List<CartItem>>(emptyList())
@@ -12,8 +22,27 @@ class CartViewModel {
     
     init {
         // Mock 20 items
-        val mockItems = (1..20).map { i -> CartItem(id = i.toString(), name = "Item $i", price = 10.0 + i) }
-        _cartItems.value = mockItems
+        viewModelScope.launch {
+            fetchProducts()
+        }
+    }
+
+    private suspend fun fetchProducts() {
+        val client = HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(Json {
+                    ignoreUnknownKeys = true
+                })
+            }
+        }
+        try {
+            val response: List<Product> = client.get("https://fakestoreapi.com/products").body()
+            val cartItems = response.map { product -> CartItem(id = product.id.toString(), name = product.title, price = product.price) }
+            _cartItems.value = cartItems
+        } catch (e: Exception) {
+            // Handle exceptions, maybe log or show an error message
+            e.printStackTrace()
+        }
     }
 
 
@@ -33,3 +62,10 @@ class CartViewModel {
         _cartItems.value = emptyList()
     }
 }
+
+@Serializable
+data class Product(
+    val id: Int,
+    val title: String,
+    val price: Double
+)
