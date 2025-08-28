9cbe031f4882ad522d1298696269b45c334d4975
dengfeng.yan <dengfeng.yan@iherb.com>
2025-08-28 14:53:11 +0000

    feat: modified product card

--- app/src/main/java/com/example/ynsdemo/cart/CartScreen.kt
+++ app/src/main/java/com/example/ynsdemo/cart/CartScreen.kt
@@ -1,13 +1,17 @@
 package com.example.ynsdemo.cart
 
 import CartViewModel
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.material3.Text
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
@@ -15,7 +19,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -29,7 +38,41 @@ fun CartScreen() {
             modifier = Modifier
                 .fillMaxWidth()
                 .weight(1.0f)
-        ) { items(cartItems) { item -> Text("${item.name} - ${item.price}") } }
+        ) {
+ items(cartItems) { item ->
+                CartItem(item = item)
+            }
+        }
+
+@Composable
+fun CartItem(item: CartItem) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.LightGray)
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        AsyncImage(
+            model = item.fontImg,
+            contentDescription = item.name,
+            modifier = Modifier.size(64.dp),
+            contentScale = ContentScale.Fit
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(item.name)
+            Text("Price: ${item.price}")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewCartScreen() {
+    CartScreen()
+}
+
         Row(
             modifier = Modifier.fillMaxWidth().height(64.dp),
             horizontalArrangement = Arrangement.SpaceBetween,
